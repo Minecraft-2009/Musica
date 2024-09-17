f7a50081ec141d1a69bc158e8db265d974a9303e
Tomas Roth <124087796+Minecraft-2009@users.noreply.github.com>
2024-09-17 18:57:33 +0000

    Added code

--- app.py
+++ app.py
@@ -7,7 +7,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-  return '<b>Hello</b>'
+  return '<b>Hello</b>'\
+    '<br>'\
+    '<body>'\
+    '<a href="/PinkFloyd/Meddle/Echoes">Echoes</a>'\
+    '</body>'
   
+@app.route('/PinkFloyd/Meddle/Echoes')
+def PinkFloyd_Meddle_Echoes():
+   return render_template('play.html')
+
+# @app.route('/PinkFloyd/Meddle/Echoes.png')
+# def PFechoesMP3():
+#    return '/workspaces/Musica/PinkFloyd/Meddle/ Echoes.mp3'
+
 if __name__ == '__main__':
     app.run(debug=True)
